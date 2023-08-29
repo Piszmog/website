487c9fc77b36827de1fa053feecdd2edfe4ac166
Randell <piszmogcode@gmail.com>
2023-08-29 11:42:52 -0600

    Support dark mode

--- src/lib/components/ToolboxCard.svelte
+++ src/lib/components/ToolboxCard.svelte
@@ -35,16 +35,16 @@
 
 <li class="col-span-1 flex rounded-md shadow-sm">
 	<div
-		class="flex w-16 flex-shrink-0 items-center justify-center rounded-l-md text-sm font-medium text-white {colorClass}"
+		class="dark flex w-16 flex-shrink-0 items-center justify-center rounded-l-md text-sm font-medium text-white {colorClass}"
 	>
 		{capitalize(title)}
 	</div>
 	<div
-		class="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white"
+		class="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white dark:border-slate-600 dark:bg-slate-800"
 	>
 		<div class="flex-1 truncate px-4 py-2 text-sm">
 			{#each items as item, index (index)}
-				<p class="text-gray-500">{item}</p>
+				<p class="text-gray-500 dark:text-slate-300">{item}</p>
 			{/each}
 		</div>
 	</div>
