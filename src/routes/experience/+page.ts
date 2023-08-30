8111802a3ed3735fe65e2293ab6586b8daace00e
Randell <piszmogcode@gmail.com>
2023-08-30 09:24:54 -0600

    Fix import

--- src/routes/experience/+page.ts
+++ src/routes/experience/+page.ts
@@ -1,4 +1,4 @@
-import type { PageLoad } from '../../../.svelte-kit/types/src/routes/experience/$types';
+import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
 	const res = await fetch('/experience.json');
