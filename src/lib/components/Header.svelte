64b94048b34d82ce07df63dff623f3e6cc639ff7
Randell <piszmogcode@gmail.com>
2023-08-30 10:14:40 -0600

    Add projects menu on mobile

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -131,6 +131,12 @@
 							>
 								Experience
 							</a>
+							<a
+								href="/projects"
+								class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:text-white"
+							>
+								Projects
+							</a>
 						</div>
 						<div class="py-6">
 							<DarkModeButton />
