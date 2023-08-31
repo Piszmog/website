1103e5ce7a300a873838d2f386df7558e84a10ff
Randell <piszmogcode@gmail.com>
2023-08-31 17:26:14 -0600

    Add an explicit height and width

--- src/lib/components/LinkHome.svelte
+++ src/lib/components/LinkHome.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import avatar from '$lib/assets/avatar.png?format=webp';
+	import avatar from '$lib/assets/avatar.png?height=40&width=40&format=webp';
 </script>
 
 <a href="/" class="-m-1.5 p-1.5">
 	<span class="sr-only">Home</span>
-	<img class="h-10 w-auto rounded-xl" src={avatar} alt="" />
+	<img class="h-10 w-auto rounded-xl" height="40" width="40" src={avatar} alt="" />
 </a>
